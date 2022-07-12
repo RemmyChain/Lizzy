d6321d037858a2b6f79c81a50b4808f8b8764952
RemmyChain <remcoarjanketting@gmail.com>
2022-07-12 15:28:05 +0200

    changing file structure and cleaning up

--- images.py
+++ images.py
@@ -1,79 +1,84 @@
 import pygame
+import os
 
-rocktile = pygame.image.load("rockblock.png").convert_alpha()
+rocktile = pygame.image.load(os.path.join('images', 'rockblock.png')).convert_alpha()
 
-tracer = pygame.image.load("tracer.png").convert_alpha()
+tracer = pygame.image.load(os.path.join('images', 'tracer.png')).convert_alpha()
 
 hitpix = [
-    pygame.image.load("hit1.png").convert_alpha(),
-    pygame.image.load("hit2.png").convert_alpha(),
-    pygame.image.load("hit3.png").convert_alpha(),
-    pygame.image.load("hit4.png").convert_alpha(),
-    pygame.image.load("hit5.png").convert_alpha()
+    pygame.image.load(os.path.join('images', 'hit1.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'hit2.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'hit3.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'hit4.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'hit5.png')).convert_alpha(),
 
 ]
 
 barrelpix = [
-    pygame.image.load("barrels01.png").convert_alpha(),
-    pygame.image.load("barrels02.png").convert_alpha(),
-    pygame.image.load("barrels03.png").convert_alpha(),
-    pygame.image.load("barrels04.png").convert_alpha(),
-    pygame.image.load("barrels05.png").convert_alpha(),
-    pygame.image.load("barrels06.png").convert_alpha(),
+    pygame.image.load(os.path.join('images', 'barrels01.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'barrels02.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'barrels03.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'barrels04.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'barrels05.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'barrels06.png')).convert_alpha(),
+
 ]
 
 groundtiles = [
-    pygame.image.load("groundlefttop.png").convert_alpha(),
-    pygame.image.load("groundmidtop.png").convert(),
-    pygame.image.load("groundbotleft.png").convert(),
-    pygame.image.load("groundbotmid.png").convert(),
-    pygame.transform.flip((pygame.image.load("groundlefttop.png").convert_alpha()), True, False),
-    pygame.transform.flip((pygame.image.load("groundbotleft.png").convert()), True, False)
+    pygame.image.load(os.path.join('images', 'groundlefttop.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'groundmidtop.png')).convert(),
+    pygame.image.load(os.path.join('images', 'groundbotleft.png')).convert(),
+    pygame.image.load(os.path.join('images', 'groundbotmid.png')).convert(),
+
+    pygame.transform.flip((pygame.image.load(os.path.join('images', 'groundlefttop.png')).convert_alpha()), True, False),
+    pygame.transform.flip((pygame.image.load(os.path.join('images', 'groundbotleft.png')).convert()), True, False)
 ]
 
-Lizlegstill = pygame.image.load("walkstilla.png").convert_alpha()
-Liztorsostill = pygame.image.load("torso01a.png").convert_alpha()
+Lizlegstill = pygame.image.load(os.path.join('images', 'walkstilla.png')).convert_alpha()
+Liztorsostill = pygame.image.load(os.path.join('images', 'torso01a.png')).convert_alpha()
 Lizlegsair = [
-    pygame.image.load("jump1.png").convert_alpha(),
-    pygame.image.load("jump2.png").convert_alpha()
+    pygame.image.load(os.path.join('images', 'jump1.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'jump2.png')).convert_alpha(),
 ]
 Lizlegsaireverse = [
-    pygame.transform.flip((pygame.image.load("jump1.png").convert_alpha()), True, False),
-    pygame.transform.flip((pygame.image.load("jump2.png").convert_alpha()), True, False)
+    pygame.transform.flip((pygame.image.load(os.path.join('images', 'jump1.png')).convert_alpha()), True, False),
+    pygame.transform.flip((pygame.image.load(os.path.join('images', 'jump2.png')).convert_alpha()), True, False)
 ]
 Lizlegsrun = [
-    pygame.image.load("walk01a.png").convert_alpha(),
-    pygame.image.load("walk02a.png").convert_alpha(),
-    pygame.image.load("walk03a.png").convert_alpha(),
-    pygame.image.load("walk04a.png").convert_alpha(),
-    pygame.image.load("walk05a.png").convert_alpha(),
-    pygame.image.load("walk06a.png").convert_alpha(),
-    pygame.image.load("walk07a.png").convert_alpha(),
-    pygame.image.load("walk08a.png").convert_alpha(),
-    pygame.image.load("walk09a.png").convert_alpha(),
-    pygame.image.load("walk10a.png").convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk01a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk02a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk03a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk04a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk05a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk06a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk07a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk08a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk09a.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'walk10a.png')).convert_alpha(),
+
 ]
 Lizlegsreverse = []
 for i in range(len(Lizlegsrun)):
     Lizlegsreverse.append(pygame.transform.flip((Lizlegsrun[i]), True, False))
 
 Liztorsopix = [
-    pygame.image.load("torsosmall00.png").convert_alpha(),
-    pygame.image.load("torsosmall01.png").convert_alpha(),
-    pygame.image.load("torsosmall02.png").convert_alpha(),
-    pygame.image.load("torsosmall03.png").convert_alpha(),
-    pygame.image.load("torsosmall04.png").convert_alpha(),
-    pygame.image.load("torsosmall05.png").convert_alpha(),
-    pygame.image.load("torsosmall06.png").convert_alpha(),
-    pygame.image.load("torsosmall07.png").convert_alpha(),
-    pygame.image.load("torsosmall08.png").convert_alpha(),
-    pygame.image.load("torsosmall09.png").convert_alpha(),
-    pygame.image.load("torsosmall10.png").convert_alpha(),
-    pygame.image.load("torsosmall11.png").convert_alpha(),
-    pygame.image.load("torsosmall12.png").convert_alpha(),
-    pygame.image.load("torsosmall13.png").convert_alpha(),
-    pygame.image.load("torsosmall14.png").convert_alpha(),
-    pygame.image.load("torsosmall15.png").convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall00.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall01.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall02.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall03.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall04.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall05.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall06.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall07.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall08.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall09.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall10.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall11.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall12.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall13.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall14.png')).convert_alpha(),
+    pygame.image.load(os.path.join('images', 'torsosmall15.png')).convert_alpha(),
+
 ]
 Liztorsoreverse = []
 for i in range(len(Liztorsopix)):
--- initialising.py
+++ initialising.py
@@ -1,15 +1,16 @@
 import pygame
 from pygame import *
+import os
 
 pygame.init()
 
 flags = FULLSCREEN | DOUBLEBUF
 screen = pygame.display.set_mode((0, 0), flags, 16)
-background = pygame.image.load("bg01.jpg").convert()
+background = pygame.image.load(os.path.join('images', 'bg01.jpg')).convert()
 running = True
 vec = pygame.math.Vector2
 # pygame.mouse.set_visible(False)
-mousesurf = pygame.image.load("reticle.png")
+mousesurf = pygame.image.load(os.path.join('images', 'reticle.png'))
 reticursor = pygame.cursors.Cursor((15,15), mousesurf)
 pygame.mouse.set_cursor(reticursor)
 font = pygame.font.SysFont("comicsansms", 20)

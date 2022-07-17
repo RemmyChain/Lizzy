e1acb408494475ce4c24f91c95b8c8fafca8f045
RemmyChain <remcoarjanketting@gmail.com>
2022-07-17 15:45:13 +0200

    The kameker walks! XD

--- enemies.py
+++ enemies.py
@@ -28,6 +28,16 @@ class kamaker(pygame.sprite.Sprite):
         self.reversed = False
         self.timer = 0
         self.oldpos = self.pos
+        self.xpos1 = 40
+        self.xpos2 = 85
+        self.xpos3 = 100
+        self.xpos4 = 145
+        self.ypos1 = 70
+        self.ypos2 = 70
+        self.ypos3 = 70
+        self.ypos4 = 70
+
+
 
     def gethit(self):
         self.health -= 1
@@ -89,24 +99,83 @@ class kamaker(pygame.sprite.Sprite):
 
     def hardblockscheck(self):
         hits = pygame.sprite.spritecollide(self, hardblocks, False)
+        if hits:
+            self.pos = self.oldpos
+            if not self.reversed:
+                self.reversed = True
+            elif self.reversed:
+                self.reversed = False
     def render(self):
+
         if not self.reversed:
             screen.blit(self.surf, self.rect)
         else:
             flipped = pygame.transform.flip(self.surf, True, False)
             screen.blit(flipped, self.rect)
+        self.surf.fill((0, 0, 0))
     def update(self):
+        if self.timer > 19:
+            self.timer = 0
         self.edgedetect()
         self.move()
         self.platformcheck()
         self.hardblockscheck()
         self.animate()
         self.render()
+        self.timer += 1
+
     def animate(self):
-        self.surf.blit(kamakerpaw, (70, 70))
-        self.surf.blit(kamakerpaw, (140, 70))
+        self.frontpawfront()
+        self.frontpawback()
+        self.backpawfront()
+        self.backpawback()
+        self.surf.blit(kamakerpaw, (self.xpos1, self.ypos1))
+        self.surf.blit(kamakerpaw, (self.xpos3, self.ypos3))
         self.surf.blit(kamakerbody, (50,15))
         self.surf.blit(kamakertail, (160,50))
-        self.surf.blit(kamakerpaw, (50,70))
-        self.surf.blit(kamakerpaw, (120, 70))
+        self.surf.blit(kamakerpaw, (self.xpos2, self.ypos2))
+        self.surf.blit(kamakerpaw, (self.xpos4, self.ypos4))
         self.surf.blit(kamakerhead, (0,0))
+
+    def frontpawfront(self):
+        if self.timer < 10:
+            self.xpos1 += 5
+        elif self.timer >= 10 and self.timer < 15:
+            self.xpos1 -= 5
+            self.ypos1 -= 5
+        elif self.timer >= 15:
+            self.xpos1 -= 5
+            self.ypos1 += 5
+
+    def frontpawback(self):
+        if self.timer < 5:
+            self.xpos2 -= 5
+            self.ypos2 -=5
+        elif self.timer >= 5 and self.timer < 10:
+            self.xpos2 -= 5
+            self.ypos2 += 5
+        elif self.timer >= 10:
+            self.xpos2 += 5
+
+    def backpawfront(self):
+        if self.timer < 10:
+            self.xpos3 += 5
+        elif self.timer >= 10 and self.timer < 15:
+            self.xpos3 -= 5
+            self.ypos3 -= 5
+        elif self.timer >= 15:
+            self.xpos3 -= 5
+            self.ypos3 += 5
+
+    def backpawback(self):
+        if self.timer < 5:
+            self.xpos4 -= 5
+            self.ypos4 -=5
+        elif self.timer >= 5 and self.timer < 10:
+            self.xpos4 -= 5
+            self.ypos4 += 5
+        elif self.timer >= 10:
+            self.xpos4 += 5
+
+
+
